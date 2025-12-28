5874e048b127f440faab15d7dc362a8314306981
zeamanuel fetene <zeamanuelfetene1994@gmail.com>
2025-12-28 06:35:19 -0800

    Enhance CORS configuration to restrict access to specified frontend URL and enable credentials support.

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors())
+app.use(cors({
+  origin: process.env.FRONTEND_URL, // only allow your frontend
+  credentials: true, // optional, if you send cookies
+}));
+
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true, limit: "10mb" }))
 
